cbe9e7dbb99ef9c68479306b918c5e86dab9c008
shahab-github <113800863+shahab-github@users.noreply.github.com>
2023-08-26 17:36:24 -0400

    Create vpc_test.go

---
+++ tests/vpc_test.go
@@ -0,0 +1,29 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/aws"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestVpc(t *testing.T) {
+	terraformOptions := &terraform.Options{
+		TerraformDir: "../examples/vpc",
+	}
+
+	defer terraform.Destroy(t, terraformOptions)
+
+	terraform.InitAndApply(t, terraformOptions)
+
+	vpcID := terraform.Output(t, terraformOptions, "vpc_id")
+
+	awsRegion := aws.GetRandomStableRegion(t, nil, nil)
+
+	vpc := aws.GetVpcById(t, vpcID, awsRegion)
+
+	assert.Equal(t, "10.0.0.0/16", vpc.CidrBlock)
+	assert.Equal(t, 1, len(vpc.Subnets))
+	assert.Equal(t, "us-west-2a", vpc.Subnets[0].AvailabilityZone)
+}
